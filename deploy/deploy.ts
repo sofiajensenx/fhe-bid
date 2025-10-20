f1489e4a49a7fb960fd6e02ceb4a1348426f2d01
sofiajensenx <bxzgiks561@hotmail.com>
2025-10-20 20:20:31 +0800

    update deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,17 +1,21 @@
+import * as dotenv from "dotenv";
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+dotenv.config();
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const encryptedAuctionDeployment = await deploy("EncryptedAuction", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`EncryptedAuction contract:`, encryptedAuctionDeployment.address);
 };
+
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_encryptedAuction"; // id required to prevent reexecution
+func.tags = ["EncryptedAuction"];
